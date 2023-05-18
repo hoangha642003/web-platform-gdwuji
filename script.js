33d07179cc5d5f84938af2f8222e881fa640bc61
hoangha642003 <129708029+hoangha642003@users.noreply.github.com>
2023-05-18 22:52:40 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//let feet=1.0;
+//let meter=0.305;
+//footToMeter(feet)
+//function footToMeter(feet)
+//{
+ // meter = 0.305 * feet
+  //document.write(meter)
+//}
+///
+let feet=3.279;
+let meter=60.0;
+footToMeter(meter)
+function footToMeter(meter)
+{
+  feet = 3.279 * meter
+  document.write(feet)
+}
+
+
+
+
+
